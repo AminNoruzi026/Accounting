a5219f0407723f53c4c4a4d6e14965008de63854
Mohammad Amin Noruzi <74448298+AminNoruzi026@users.noreply.github.com>
2024-03-12 07:29:40 +0330

    Last Update night

--- Accounting.App/Properties/AssemblyInfo.cs
+++ Accounting.App/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Accounting.App")]
+[assembly: AssemblyTitle("Accounting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Accounting.App")]
+[assembly: AssemblyProduct("Accounting")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
